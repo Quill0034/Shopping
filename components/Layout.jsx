b83ba29ab46abfaaeb2dbeb2688cea0ac57e9b3b
Quil <quyhoangtran0034@gmail.com>
2022-10-12 18:50:54 -0500

    add .js to file path

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Head from 'next/head';
 
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Navbar from './Navbar.js';
+import Footer from './Footer.js';
 
 
 const Layout = ({ children }) => {
